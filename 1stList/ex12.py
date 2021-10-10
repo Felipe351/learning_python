b7d0367f3364aa3cfed10766ca442797bb9ae324
Felipe <feliperodrigues011284@gmail.com>
2021-10-10 01:51:55 -0300

    Alteração de código do exercício 12

--- 1stList/ex12.py
+++ 1stList/ex12.py
@@ -1,1 +1,7 @@
-#Make a programm that 
+#Considering someone's statue, make a programm able to calculate the ideal weight from the person.
+#   Use this formula: (72.7 * altura) - 58
+
+statue = float(input("Inform someone's statue: "))
+ideal_weight = (statue * 72.7) - 58
+
+print(f'A {statue}cm person shall weight {ideal_weight} kilos')
